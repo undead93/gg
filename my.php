28f818c826e4cb92ea6b87fb08f70ded31cea259
undead93 <syafiq1995det@gmail.com>
2018-12-14 19:55:21 +0800

    cc

--- my.php
+++ my.php
@@ -9,10 +9,10 @@
 ///Capture dulu apk nya ambil data yang di butuhkan
 
 $ticket =
-"sHp2r312otmCpnGns3mCqJhovNiBh5mWr-CGo7PLc8-xsIpqiZyrzYKUdWC1hYaekKDNmH-Hndu70KSis7WLza2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+"sHp2r312otmCpnGns3mCqJhovNiBh4eXu62Br7O4dpO8o3pqiZ-mk4OpdWS_Y3mrhnq_3X6ti9u6mH6gtMuCl62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
 
 $meid = "272a01dd25720f72";
 
 $os="android";
 
-$sign="1d683e44ac5cf77e48e0d8c0e50926a32604c13c1c992ce46fb4548922010b29";
+$sign="2dec4d7b6f926a5b0a069e6eb4216b14e3b78b269c5f6451748fea6ded001bc6";

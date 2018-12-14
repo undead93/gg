6506dc77dcc82fd7e1ecb2160667e57e29ba473a
undead93 <syafiq1995det@gmail.com>
2018-12-14 15:16:26 +0800

    ff

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket = 
+"sHp2r312otmCpnGns3mCqJhovNiBh4PbruCJa7_Lmcuwenukf4a22YPMgpqzm4Kbhoq8zIugf9u6up-wv7h6262jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+
+$meid = "272a01dd25720f72";
+
+$os="android";
+
+$sign="2f7418515b8624ab46b45b09fb8d4080a9973109f8736a15f98a8f1dd04ee222";

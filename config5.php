2ebf0d2d0ef514c88d990b82e651694f3a36b2a5
undead93 <syafiq1995det@gmail.com>
2018-12-15 20:16:44 +0800

    ff

---
+++ config5.php
@@ -0,0 +1,18 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket =
+"sHp2r312otmCpnGns3mCqJhovNiBh4jOr6qFbLTbcpayjYejfnm22oK5epizhXGpkYq7lX-df5iv05Fstbh_zK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+
+$meid = "272a01dd25720f72";
+
+$os="android";
+
+$sign="e8ea830450b4456b6f85457c06ee74af6244deab3c6f650828a5903435ba0262";
---
+++ config6.php
@@ -0,0 +1,18 @@
+<?php
+
+///Jangan hapus tanpa kutip nya(")
+
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+
+///Ganti xxxxxxx dengan data punya kalian
+
+///Capture dulu apk nya ambil data yang di butuhkan
+
+$ticket =
+"sHp2r312otmCpnGns3mCqJhovNiBh3zOr9CgpMC4h8qyin-gfYmry47chpqzhYFmhn2wzIudmsy7vY2wwMtzz62jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+
+$meid = "272a01dd25720f72";
+
+$os="android";
+
+$sign="404f639a733ba77fb93f995c7cae8ca7ac86c44f7133201c8a135bc5637130a2";
